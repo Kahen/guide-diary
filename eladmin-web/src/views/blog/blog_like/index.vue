c158e9444cd638fe6264889bdc141b7d414994e7
lijiaxing <kahenlee@163.com>
2020-12-09 17:43:41 +0800

    fix

--- eladmin-web/src/views/blog/blog_like/index.vue
+++ eladmin-web/src/views/blog/blog_like/index.vue
@@ -2,27 +2,77 @@
   <div class="app-container">
     <!--工具栏-->
     <div class="head-container">
+      <div v-if="crud.props.searchToggle">
+        <!-- 搜索 -->
+        <label class="el-form-item-label">点赞ID</label>
+        <el-input
+          v-model="query.likeId"
+          class="filter-item"
+          clearable
+          placeholder="点赞ID"
+          style="width: 185px;"
+          @keyup.enter.native="crud.toQuery"
+        />
+        <label class="el-form-item-label">博客ID</label>
+        <el-input
+          v-model="query.blogId"
+          class="filter-item"
+          clearable
+          placeholder="博客ID"
+          style="width: 185px;"
+          @keyup.enter.native="crud.toQuery"
+        />
+        <label class="el-form-item-label">点赞用户ID</label>
+        <el-input
+          v-model="query.userId"
+          class="filter-item"
+          clearable
+          placeholder="点赞用户ID"
+          style="width: 185px;"
+          @keyup.enter.native="crud.toQuery"
+        />
+        <label class="el-form-item-label">创建时间</label>
+        <el-input
+          v-model="query.createTime"
+          class="filter-item"
+          clearable
+          placeholder="创建时间"
+          style="width: 185px;"
+          @keyup.enter.native="crud.toQuery"
+        />
+        <rrOperation :crud="crud" />
+      </div>
       <!--如果想在工具栏加入更多按钮，可以使用插槽方式， slot = 'left' or 'right'-->
-      <crudOperation :permission="permission"/>
+      <crudOperation :permission="permission" />
       <!--表单组件-->
-      <el-dialog :before-close="crud.cancelCU" :close-on-click-modal="false" :title="crud.status.title"
-                 :visible.sync="crud.status.cu > 0" width="500px">
+      <el-dialog
+        :before-close="crud.cancelCU"
+        :close-on-click-modal="false"
+        :title="crud.status.title"
+        :visible.sync="crud.status.cu > 0"
+        width="500px"
+      >
         <el-form ref="form" :model="form" :rules="rules" label-width="80px" size="small">
           <el-form-item
-            label="点赞ID" prop="likeId">
-            <el-input v-model="form.likeId" style="width: 370px;"/>
+            label="点赞ID"
+          >
+            <el-input v-model="form.likeId" style="width: 370px;" />
           </el-form-item>
           <el-form-item
-            label="博客ID" prop="blogId">
-            <el-input v-model="form.blogId" style="width: 370px;"/>
+            label="博客ID"
+          >
+            <el-input v-model="form.blogId" style="width: 370px;" />
           </el-form-item>
           <el-form-item
-            label="点赞用户ID" prop="userId">
-            <el-input v-model="form.userId" style="width: 370px;"/>
+            label="点赞用户ID"
+          >
+            <el-input v-model="form.userId" style="width: 370px;" />
           </el-form-item>
           <el-form-item
-            label="创建时间" prop="createTime">
-            <el-input v-model="form.createTime" style="width: 370px;"/>
+            label="创建时间"
+            prop="createTime"
+          >
+            <el-date-picker v-model="form.createTime" style="width: 370px;" type="datetime" />
           </el-form-item>
         </el-form>
         <div slot="footer" class="dialog-footer">
@@ -31,23 +81,41 @@
         </div>
       </el-dialog>
       <!--表格渲染-->
-      <el-table ref="table" v-loading="crud.loading" :data="crud.data" size="small" style="width: 100%;"
-                @selection-change="crud.selectionChangeHandler">
-        <el-table-column type="selection" width="55"/>
-        <el-table-column label="点赞ID"
-                         prop="likeId"/>
-        <el-table-column label="博客ID"
-                         prop="blogId"/>
-        <el-table-column label="点赞用户ID"
-                         prop="userId"/>
-        <el-table-column label="创建时间"
-                         prop="createTime">
+      <el-table
+        ref="table"
+        v-loading="crud.loading"
+        :data="crud.data"
+        size="small"
+        style="width: 100%;"
+        @selection-change="crud.selectionChangeHandler"
+      >
+        <el-table-column type="selection" width="55" />
+        <el-table-column
+          label="点赞ID"
+          prop="likeId"
+        />
+        <el-table-column
+          label="博客ID"
+          prop="blogId"
+        />
+        <el-table-column
+          label="点赞用户ID"
+          prop="userId"
+        />
+        <el-table-column
+          label="创建时间"
+          prop="createTime"
+        >
           <template slot-scope="scope">
             <span>{{ parseTime(scope.row.createTime) }}</span>
           </template>
         </el-table-column>
-        <el-table-column v-permission="['admin','like:edit','like:del']" align="center"
-                         label="操作" width="150px">
+        <el-table-column
+          v-permission="['admin','like:edit','like:del']"
+          align="center"
+          label="操作"
+          width="150px"
+        >
           <template slot-scope="scope">
             <udOperation
               :data="scope.row"
@@ -57,22 +125,23 @@
         </el-table-column>
       </el-table>
       <!--分页组件-->
-      <pagination/>
+      <pagination />
     </div>
   </div>
 </template>
 
 <script>
-import CRUD, {crud, form, header, presenter} from '@crud/crud'
+import crudLike from '@/api/blog/like'
+import CRUD, { crud, form, header, presenter } from '@crud/crud'
 import rrOperation from '@crud/RR.operation'
 import crudOperation from '@crud/CRUD.operation'
 import udOperation from '@crud/UD.operation'
 import pagination from '@crud/Pagination'
 
-const defaultForm = {likeId: null, blogId: null, userId: null, createTime: null}
+const defaultForm = { likeId: null, blogId: null, userId: null, createTime: null }
 export default {
-  name: 'CommentLike',
-  components: {pagination, crudOperation, rrOperation, udOperation},
+  name: 'Like',
+  components: { pagination, crudOperation, rrOperation, udOperation },
   mixins: [presenter(), header(), form(defaultForm), crud()],
   cruds() {
     return CRUD({
@@ -80,7 +149,7 @@ export default {
       url: 'api/like',
       idField: 'likeId',
       sort: 'likeId,desc',
-      crudMethod: {...crudLike}
+      crudMethod: { ...crudLike }
     })
   },
   data() {
@@ -91,19 +160,28 @@ export default {
         del: ['admin', 'like:del']
       },
       rules: {
-        likeId: [
-          {required: true, message: '点赞ID不能为空', trigger: 'blur'}
-        ],
-        blogId: [
-          {required: true, message: '博客ID不能为空', trigger: 'blur'}
-        ],
-        userId: [
-          {required: true, message: '点赞用户ID不能为空', trigger: 'blur'}
-        ],
         createTime: [
-          {required: true, message: '创建时间不能为空', trigger: 'blur'}
+          { required: true, message: '创建时间不能为空', trigger: 'blur' }
         ]
-      }
+      },
+      queryTypeOptions: [
+        {
+          key: 'likeId',
+          display_name: '点赞ID'
+        },
+        {
+          key: 'blogId',
+          display_name: '博客ID'
+        },
+        {
+          key: 'userId',
+          display_name: '点赞用户ID'
+        },
+        {
+          key: 'createTime',
+          display_name: '创建时间'
+        }
+      ]
     }
   },
   methods: {
