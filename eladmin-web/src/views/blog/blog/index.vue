2f85eb77bfc5214bcd25c25846445a7b8b379a91
lijiaxing <kahenlee@163.com>
2020-12-09 17:44:33 +0800

    fix

--- eladmin-web/src/views/blog/blog/index.vue
+++ eladmin-web/src/views/blog/blog/index.vue
@@ -5,62 +5,108 @@
       <div v-if="crud.props.searchToggle">
         <!-- 搜索 -->
         <label class="el-form-item-label">内容</label>
-        <el-input v-model="query.content" class="filter-item"
-                  clearable
-                  placeholder="内容" style="width: 185px;" @keyup.enter.native="crud.toQuery"/>
+        <el-input
+          v-model="query.content"
+          class="filter-item"
+          clearable
+          placeholder="内容"
+          style="width: 185px;"
+          @keyup.enter.native="crud.toQuery"
+        />
         <label class="el-form-item-label">视频URL</label>
-        <el-input v-model="query.videoUrl" class="filter-item"
-                  clearable
-                  placeholder="视频URL" style="width: 185px;" @keyup.enter.native="crud.toQuery"/>
+        <el-input
+          v-model="query.videoUrl"
+          class="filter-item"
+          clearable
+          placeholder="视频URL"
+          style="width: 185px;"
+          @keyup.enter.native="crud.toQuery"
+        />
         <label class="el-form-item-label">创建时间</label>
-        <el-input v-model="query.createTime" class="filter-item"
-                  clearable
-                  placeholder="创建时间" style="width: 185px;" @keyup.enter.native="crud.toQuery"/>
+        <el-input
+          v-model="query.createTime"
+          class="filter-item"
+          clearable
+          placeholder="创建时间"
+          style="width: 185px;"
+          @keyup.enter.native="crud.toQuery"
+        />
         <label class="el-form-item-label">发布时间</label>
-        <el-input v-model="query.publishTime" class="filter-item"
-                  clearable
-                  placeholder="发布时间" style="width: 185px;" @keyup.enter.native="crud.toQuery"/>
+        <el-input
+          v-model="query.publishTime"
+          class="filter-item"
+          clearable
+          placeholder="发布时间"
+          style="width: 185px;"
+          @keyup.enter.native="crud.toQuery"
+        />
         <label class="el-form-item-label">是否原创</label>
-        <el-input v-model="query.isOriginal" class="filter-item"
-                  clearable
-                  placeholder="是否原创" style="width: 185px;" @keyup.enter.native="crud.toQuery"/>
-        <rrOperation :crud="crud"/>
+        <el-input
+          v-model="query.isOriginal"
+          class="filter-item"
+          clearable
+          placeholder="是否原创"
+          style="width: 185px;"
+          @keyup.enter.native="crud.toQuery"
+        />
+        <rrOperation :crud="crud" />
       </div>
       <!--如果想在工具栏加入更多按钮，可以使用插槽方式， slot = 'left' or 'right'-->
-      <crudOperation :permission="permission"/>
+      <crudOperation :permission="permission" />
       <!--表单组件-->
-      <el-dialog :before-close="crud.cancelCU" :close-on-click-modal="false" :title="crud.status.title"
-                 :visible.sync="crud.status.cu > 0" width="500px">
+      <el-dialog
+        :before-close="crud.cancelCU"
+        :close-on-click-modal="false"
+        :title="crud.status.title"
+        :visible.sync="crud.status.cu > 0"
+        width="500px"
+      >
         <el-form ref="form" :model="form" :rules="rules" label-width="80px" size="small">
           <el-form-item
-            label="博客ID">
-            <el-input v-model="form.blogId" style="width: 370px;"/>
+            label="博客ID"
+          >
+            <el-input v-model="form.blogId" style="width: 370px;" />
           </el-form-item>
           <el-form-item
-            label="用户ID">
-            <el-input v-model="form.userId" style="width: 370px;"/>
+            label="用户ID"
+          >
+            <el-input v-model="form.userId" style="width: 370px;" />
           </el-form-item>
           <el-form-item
-            label="内容" prop="content">
-            <el-input v-model="form.content" :rows="3" style="width: 370px;"
-                      type="textarea"/>
+            label="内容"
+            prop="content"
+          >
+            <el-input
+              v-model="form.content"
+              :rows="3"
+              style="width: 370px;"
+              type="textarea"
+            />
           </el-form-item>
           <el-form-item
-            label="视频URL">
-            <el-input v-model="form.videoUrl" style="width: 370px;"/>
+            label="视频URL"
+          >
+            <el-input v-model="form.videoUrl" style="width: 370px;" />
           </el-form-item>
           <el-form-item
-            label="创建时间">
-            <el-date-picker v-model="form.createTime" style="width: 370px;" type="datetime"/>
+            label="创建时间"
+          >
+            <el-date-picker v-model="form.createTime" style="width: 370px;" type="datetime" />
           </el-form-item>
           <el-form-item
-            label="发布时间">
-            <el-date-picker v-model="form.publishTime" style="width: 370px;" type="datetime"/>
+            label="发布时间"
+          >
+            <el-date-picker v-model="form.publishTime" style="width: 370px;" type="datetime" />
           </el-form-item>
           <el-form-item
-            label="是否原创" prop="isOriginal">
-            <el-radio v-for="item in dict.is_original" :key="item.id"
-                      v-model="form.isOriginal" :label="item.value">{{ item.label }}
+            label="是否原创"
+          >
+            <el-radio
+              v-for="item in dict.is_original"
+              :key="item.id"
+              v-model="form.isOriginal"
+              :label="item.value"
+            >{{ item.label }}
             </el-radio>
           </el-form-item>
         </el-form>
@@ -70,37 +116,61 @@
         </div>
       </el-dialog>
       <!--表格渲染-->
-      <el-table ref="table" v-loading="crud.loading" :data="crud.data" size="small" style="width: 100%;"
-                @selection-change="crud.selectionChangeHandler">
-        <el-table-column type="selection" width="55"/>
-        <el-table-column label="博客ID"
-                         prop="blogId"/>
-        <el-table-column label="用户ID"
-                         prop="userId"/>
-        <el-table-column label="内容"
-                         prop="content"/>
-        <el-table-column label="视频URL"
-                         prop="videoUrl"/>
-        <el-table-column label="创建时间"
-                         prop="createTime">
+      <el-table
+        ref="table"
+        v-loading="crud.loading"
+        :data="crud.data"
+        size="small"
+        style="width: 100%;"
+        @selection-change="crud.selectionChangeHandler"
+      >
+        <el-table-column type="selection" width="55" />
+        <el-table-column
+          label="博客ID"
+          prop="blogId"
+        />
+        <el-table-column
+          label="用户ID"
+          prop="userId"
+        />
+        <el-table-column
+          label="内容"
+          prop="content"
+        />
+        <el-table-column
+          label="视频URL"
+          prop="videoUrl"
+        />
+        <el-table-column
+          label="创建时间"
+          prop="createTime"
+        >
           <template slot-scope="scope">
             <span>{{ parseTime(scope.row.createTime) }}</span>
           </template>
         </el-table-column>
-        <el-table-column label="发布时间"
-                         prop="publishTime">
+        <el-table-column
+          label="发布时间"
+          prop="publishTime"
+        >
           <template slot-scope="scope">
             <span>{{ parseTime(scope.row.publishTime) }}</span>
           </template>
         </el-table-column>
-        <el-table-column label="是否原创"
-                         prop="isOriginal">
+        <el-table-column
+          label="是否原创"
+          prop="isOriginal"
+        >
           <template slot-scope="scope">
             {{ dict.label.is_original[scope.row.isOriginal] }}
           </template>
         </el-table-column>
-        <el-table-column v-permission="['admin','blog:edit','blog:del']" align="center"
-                         label="操作" width="150px">
+        <el-table-column
+          v-permission="['admin','blog:edit','blog:del']"
+          align="center"
+          label="操作"
+          width="150px"
+        >
           <template slot-scope="scope">
             <udOperation
               :data="scope.row"
@@ -110,14 +180,14 @@
         </el-table-column>
       </el-table>
       <!--分页组件-->
-      <pagination/>
+      <pagination />
     </div>
   </div>
 </template>
 
 <script>
 import crudBlog from '@/api/blog/blog'
-import CRUD, {crud, form, header, presenter} from '@crud/crud'
+import CRUD, { crud, form, header, presenter } from '@crud/crud'
 import rrOperation from '@crud/RR.operation'
 import crudOperation from '@crud/CRUD.operation'
 import udOperation from '@crud/UD.operation'
@@ -134,7 +204,7 @@ const defaultForm = {
 }
 export default {
   name: 'Blog',
-  components: {pagination, crudOperation, rrOperation, udOperation},
+  components: { pagination, crudOperation, rrOperation, udOperation },
   mixins: [presenter(), header(), form(defaultForm), crud()],
   dicts: ['is_original'],
   cruds() {
@@ -143,7 +213,7 @@ export default {
       url: 'api/blog',
       idField: 'blogId',
       sort: 'blogId,desc',
-      crudMethod: {...crudBlog}
+      crudMethod: { ...crudBlog }
     })
   },
   data() {
@@ -155,10 +225,7 @@ export default {
       },
       rules: {
         content: [
-          {required: true, message: '内容不能为空', trigger: 'blur'}
-        ],
-        isOriginal: [
-          {required: true, message: '是否原创不能为空', trigger: 'blur'}
+          { required: true, message: '内容不能为空', trigger: 'blur' }
         ]
       },
       queryTypeOptions: [
