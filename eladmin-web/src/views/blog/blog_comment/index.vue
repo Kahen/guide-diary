bdd60486a46a1940fe712b4d65d8cdb46940a892
lijiaxing <kahenlee@163.com>
2020-12-09 17:44:13 +0800

    fix

--- eladmin-web/src/views/blog/blog_comment/index.vue
+++ eladmin-web/src/views/blog/blog_comment/index.vue
@@ -4,37 +4,37 @@
     <div class="head-container">
       <div v-if="crud.props.searchToggle">
         <!-- 搜索 -->
-        <label class="el-form-item-label">评论ID</label>
-        <el-input
-          v-model="query.id"
-          class="filter-item"
-          clearable
-          placeholder="评论ID"
-          style="width: 185px;"
-          @keyup.enter.native="crud.toQuery"
-        />
         <label class="el-form-item-label">博客ID</label>
         <el-input
           v-model="query.blogId"
+          placeholder="博客ID"
           class="filter-item"
           clearable
-          placeholder="博客ID"
           style="width: 185px;"
           @keyup.enter.native="crud.toQuery"
         />
         <label class="el-form-item-label">评论用户ID</label>
         <el-input
           v-model="query.userId"
+          placeholder="评论用户ID"
           class="filter-item"
           clearable
-          placeholder="评论用户ID"
           style="width: 185px;"
           @keyup.enter.native="crud.toQuery"
         />
-        <rrOperation :crud="crud"/>
+        <label class="el-form-item-label">内容</label>
+        <el-input
+          v-model="query.content"
+          placeholder="内容"
+          class="filter-item"
+          clearable
+          style="width: 185px;"
+          @keyup.enter.native="crud.toQuery"
+        />
+        <rrOperation :crud="crud" />
       </div>
       <!--如果想在工具栏加入更多按钮，可以使用插槽方式， slot = 'left' or 'right'-->
-      <crudOperation :permission="permission"/>
+      <crudOperation :permission="permission" />
       <!--表单组件-->
       <el-dialog
         :before-close="crud.cancelCU"
@@ -44,26 +44,46 @@
         width="500px"
       >
         <el-form ref="form" :model="form" :rules="rules" label-width="80px" size="small">
-          <el-form-item label="评论ID">
-            <el-input v-model="form.id" style="width: 370px;"/>
+          <el-form-item
+            label="博客ID"
+          >
+            <el-input v-model="form.blogId" style="width: 370px;" />
           </el-form-item>
-          <el-form-item label="博客ID">
-            <el-input v-model="form.blogId" style="width: 370px;"/>
+          <el-form-item
+            label="评论用户ID"
+          >
+            <el-input v-model="form.userId" style="width: 370px;" />
           </el-form-item>
-          <el-form-item label="评论用户ID">
-            <el-input v-model="form.userId" style="width: 370px;"/>
+          <el-form-item
+            label="该评论所在的父级ID"
+          >
+            <el-input v-model="form.pid" style="width: 370px;" />
           </el-form-item>
-          <el-form-item label="该评论所在的父级ID">
-            <el-input v-model="form.pid" style="width: 370px;"/>
+          <el-form-item
+            label="该评论回复的评论ID,没有则为0"
+          >
+            <el-input v-model="form.replyId" style="width: 370px;" />
           </el-form-item>
-          <el-form-item label="该评论回复的评论ID,没有则为0">
-            <el-input v-model="form.replyId" style="width: 370px;"/>
+          <el-form-item
+            label="内容"
+            prop="content"
+          >
+            <el-input
+              v-model="form.content"
+              :rows="3"
+              style="width: 370px;"
+              type="textarea"
+            />
           </el-form-item>
-          <el-form-item label="内容">
-            <el-input v-model="form.content" :rows="3" style="width: 370px;" type="textarea"/>
+          <el-form-item
+            label="发布时间"
+          >
+            <el-date-picker v-model="form.publishTime" style="width: 370px;" type="datetime" />
           </el-form-item>
-          <el-form-item label="发布时间">
-            <el-date-picker v-model="form.publishTime" style="width: 370px;" type="datetime"/>
+          <el-form-item
+            label="评论ID"
+          >
+            <el-input v-model="form.commentId" style="width: 370px;" />
           </el-form-item>
         </el-form>
         <div slot="footer" class="dialog-footer">
@@ -80,19 +100,45 @@
         style="width: 100%;"
         @selection-change="crud.selectionChangeHandler"
       >
-        <el-table-column type="selection" width="55"/>
-        <el-table-column label="评论ID" prop="id"/>
-        <el-table-column label="博客ID" prop="blogId"/>
-        <el-table-column label="评论用户ID" prop="userId"/>
-        <el-table-column label="该评论所在的父级ID" prop="pid"/>
-        <el-table-column label="该评论回复的评论ID,没有则为0" prop="replyId"/>
-        <el-table-column label="内容" prop="content"/>
-        <el-table-column label="发布时间" prop="publishTime">
+        <el-table-column type="selection" width="55" />
+        <el-table-column
+          label="博客ID"
+          prop="blogId"
+        />
+        <el-table-column
+          label="评论用户ID"
+          prop="userId"
+        />
+        <el-table-column
+          label="该评论所在的父级ID"
+          prop="pid"
+        />
+        <el-table-column
+          label="该评论回复的评论ID,没有则为0"
+          prop="replyId"
+        />
+        <el-table-column
+          label="内容"
+          prop="content"
+        />
+        <el-table-column
+          label="发布时间"
+          prop="publishTime"
+        >
           <template slot-scope="scope">
             <span>{{ parseTime(scope.row.publishTime) }}</span>
           </template>
         </el-table-column>
-        <el-table-column v-permission="['admin','comment:edit','comment:del']" align="center" label="操作" width="150px">
+        <el-table-column
+          label="评论ID"
+          prop="commentId"
+        />
+        <el-table-column
+          v-permission="['admin','comment:edit','comment:del']"
+          align="center"
+          label="操作"
+          width="150px"
+        >
           <template slot-scope="scope">
             <udOperation
               :data="scope.row"
@@ -102,31 +148,39 @@
         </el-table-column>
       </el-table>
       <!--分页组件-->
-      <pagination/>
+      <pagination />
     </div>
   </div>
 </template>
 
 <script>
 import crudComment from '@/api/blog/comment'
-import CRUD, {crud, form, header, presenter} from '@crud/crud'
+import CRUD, { crud, form, header, presenter } from '@crud/crud'
 import rrOperation from '@crud/RR.operation'
 import crudOperation from '@crud/CRUD.operation'
 import udOperation from '@crud/UD.operation'
 import pagination from '@crud/Pagination'
 
-const defaultForm = {id: null, blogId: null, userId: null, pid: null, replyId: null, content: null, publishTime: null}
+const defaultForm = {
+  blogId: null,
+  userId: null,
+  pid: null,
+  replyId: null,
+  content: null,
+  publishTime: null,
+  commentId: null
+}
 export default {
   name: 'Comment',
-  components: {pagination, crudOperation, rrOperation, udOperation},
+  components: { pagination, crudOperation, rrOperation, udOperation },
   mixins: [presenter(), header(), form(defaultForm), crud()],
   cruds() {
     return CRUD({
       title: 'blog_comment',
       url: 'api/comment',
-      idField: 'id',
-      sort: 'id,desc',
-      crudMethod: {...crudComment}
+      idField: 'commentId',
+      sort: 'commentId,desc',
+      crudMethod: { ...crudComment }
     })
   },
   data() {
@@ -136,11 +190,24 @@ export default {
         edit: ['admin', 'comment:edit'],
         del: ['admin', 'comment:del']
       },
-      rules: {},
+      rules: {
+        content: [
+          { required: true, message: '内容不能为空', trigger: 'blur' }
+        ]
+      },
       queryTypeOptions: [
-        {key: 'id', display_name: '评论ID'},
-        {key: 'blogId', display_name: '博客ID'},
-        {key: 'userId', display_name: '评论用户ID'}
+        {
+          key: 'blogId',
+          display_name: '博客ID'
+        },
+        {
+          key: 'userId',
+          display_name: '评论用户ID'
+        },
+        {
+          key: 'content',
+          display_name: '内容'
+        }
       ]
     }
   },
