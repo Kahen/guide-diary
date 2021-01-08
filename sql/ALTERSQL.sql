a258633b52fb7c2ae55fe08b9b23526605b8239a
lijiaxing <kahenlee@163.com>
2021-01-08 17:38:34 +0800

    fix

--- sql/ALTERSQL.sql
+++ sql/ALTERSQL.sql
@@ -1,1 +1,3 @@
 ALTER TABLE `guide_diary`.`blog_img` MODIFY COLUMN `img_url` varchar(8192) NOT NULL DEFAULT '' COMMENT '图片URL';
+ADD COLUMN `day_timestamp` bigint(20) NOT NULL COMMENT '时间索引',
+ADD INDEX `day_index`(`day_timestamp`);
