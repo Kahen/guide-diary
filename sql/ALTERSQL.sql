70407c4f68740d18e3bd94fbd6ed7a66817d0d4f
Kahen <KahenLee@163.com>
2021-02-22 10:23:35 +0800

    Delete ALTERSQL.sql

--- sql/ALTERSQL.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE `guide_diary`.`blog_img` MODIFY COLUMN `img_url` varchar(8192) NOT NULL DEFAULT '' COMMENT '图片URL';
-ADD COLUMN `day_timestamp` bigint (20) NOT NULL COMMENT '时间索引',
-ADD INDEX `day_index`(`day_timestamp`);
-ALTER TABLE `guide_diary`.`diary`
-    MODIFY COLUMN `day_timestamp` varchar(11) NOT NULL COMMENT '时间索引';
